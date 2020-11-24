e4109d68e3e4a544473493a0214b71bd8493a0fd
sarwar_sikder <amin.se.aiub@gmail.com>
2020-11-24 10:26:18 +0600

    database updated

--- user-mgt/view/home.php
+++ user-mgt/view/home.php
@@ -10,6 +10,7 @@
 </head>
 <body>
 	<h1>Welcome home, alamin</h1>
+	<a href="create.php">Create New User</a> |
 	<a href="profile.php">Profile</a> |
 	<a href="userlist.php">User List</a> |
 	<a href="../php/logout.php">logout</a>
