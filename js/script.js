ec35373daa2d1f8beb29f0d393793defe0f9addc
sarwar_sikder <amin.se.aiub@gmail.com>
2020-12-06 10:59:08 +0600

    JS intro

---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict"
+
+let a =10;
+var b = 20;
+const c=40;
+
+let student = ['alamin', 'cs', 'alamin@aiub.edu'];
+
+//console.log('test');
+//alert('test');
+//document.write('this is JS example');
+
+
+function action(){
+	var obj = document.getElementById('d1');
+
+	if(obj.innerHTML == ""){
+		obj.innerHTML = "testing JS code...";
+	}else{
+		obj.innerHTML = "";
+	}
+}
+
+function getName(){
+	let name = document.getElementById('name').value;
+	alert(name);
+	document.getElementById('head').innerHTML = name;
+}
